1fb68d942f7ea6fcd57a3af47a9acfdd239377d3
ig94 <ignatiustan94@gmail.com>
2022-07-01 22:07:09 +0800

    remove navigate

--- harry-potter/src/App.js
+++ harry-potter/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
 
 import Characters from "./components/characters/Characters";
